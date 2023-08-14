e37502923e2b7f551dc435abd486491fe7f57e12
auziqni <auziqni@gmail.com>
2023-08-14 15:02:14 +0700

    feat:  basic public
    all public child will be public

--- app/page.tsx
+++ app/page.tsx
@@ -4,6 +4,9 @@ export default function Home() {
   return (
     <main className="m-3">
       <div>
+        <Link href={"/application"}>application</Link>
+      </div>
+      <div>
         <Link href={"/basic"}>basic</Link>
       </div>
       <div>
